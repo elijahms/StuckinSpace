38d7ee578e9cf261a1da03f851675af67820d9d3
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-12-08 16:54:46 -0500

    room 2 completed

--- app/controllers/ApplicationController.rb
+++ app/controllers/ApplicationController.rb
@@ -2,14 +2,15 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   post '/newuser' do
-    new_user = User.create(
-      name: params[:username],
-      health: 3,
-      score: 10000,
-      is_dead: 0,
-      room_id: 0,
-      email: params[:email]
-    )
+    new_user =
+      User.create(
+        name: params[:username],
+        health: 3,
+        score: 10_000,
+        is_dead: 0,
+        room_id: 0,
+        email: params[:email],
+      )
     new_user.to_json
   end
 
@@ -23,4 +24,14 @@ class ApplicationController < Sinatra::Base
     items.to_json
   end
 
+  patch '/user/:id' do
+    user_details = User.find(params[:id])
+    user_details.update(room_id: params[:room_id], is_dead: true)
+    user_details.to_json
+  end
+
+  get '/allusers' do
+    all_users = User.pluck(:name)
+    all_users.to_json
+  end
 end
