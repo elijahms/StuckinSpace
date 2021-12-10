8daea240cf95a490171b4493c968b1c6b6c7d206
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-12-10 13:07:20 -0500

    bowtie 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -316,11 +316,11 @@ Item.create(
   talk_choice_2: nil,
   is_attackable: true,
   attack_response:
-    'You smash the Desk, and try to rip open the desk drawer. After a few attempts, you force it open. Inside, you see various valuables collected from dissected humans. Most notably, a Wallet and an Omega Watch.',
+    'You smash the Desk, and try to rip open the desk drawer. After a few attempts, you force it open. Inside, you see various valuables collected from dissected humans. Most notably: a Bowtie, a Wallet and an Omega Watch.',
   durability: 0.0,
   catalyst_item: 5,
   catalyst_response:
-    "You wave the guard's Keycard near the locked drawer, and it pops open. Inside, you see various valuables collected from dissected humans. Most notably, a Bowtie, a Wallet and an Omega Watch.",
+    "You wave the guard's Keycard near the locked drawer, and it pops open. Inside, you see various valuables collected from dissected humans. Most notably: a Bowtie, a Wallet and an Omega Watch.",
   exit_trigger: true,
   triggers_on: '',
   death_trigger: nil,
